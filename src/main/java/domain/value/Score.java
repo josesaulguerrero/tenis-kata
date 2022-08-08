d89d094d14c8892da450efbdfe44eb68928cb017
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-08 18:23:29 -0500

    Define Score#parse

--- src/main/java/domain/value/Score.java
+++ src/main/java/domain/value/Score.java
@@ -2,20 +2,24 @@ package domain.value;
 
 import lombok.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Score {
+    private static final Map<Integer, String> possibleScores = new HashMap<>() {{
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
+    }};
+    // ---
     private Integer value;
-
-    private Score() {
-        this.value = 0;
-    }
-
-    public Score(Integer value) {
-        this.value = value;
-    }
 
     public static Score of(Integer value) {
         return new Score(value);
@@ -23,5 +27,9 @@ public class Score {
 
     public static Score of() {
         return new Score();
+    }
+
+    public String parse() {
+        return possibleScores.get(this.value);
     }
 }
